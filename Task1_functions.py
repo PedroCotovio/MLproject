7292d4e367b0dc146e19065e880f311aaf090c53
Pedro Cotovio <pepedro97@gmail.com>
2019-12-18 11:21:29 +0000

    Binary Class Metrics & Basic Training

--- Task1_functions.py
+++ Task1_functions.py
@@ -1,13 +1,14 @@
 import numpy as np
+from scipy import interp
 import matplotlib.pyplot as plt
-from sklearn.metrics import classification_report
-from sklearn.metrics import plot_confusion_matrix, confusion_matrix
+from sklearn.metrics import plot_confusion_matrix, confusion_matrix, classification_report, auc, plot_roc_curve
 from sklearn.model_selection import RandomizedSearchCV, train_test_split, StratifiedKFold
 from sklearn.multiclass import OneVsRestClassifier
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from sklearn import svm
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.feature_selection import SelectFromModel
+from sklearn.calibration import calibration_curve
 
 #from https://www.kaggle.com/aroraaman/quadratic-kappa-metric-explained-in-5-simple-steps
 def quadratic_kappa(actuals, preds, N=5):
@@ -88,7 +89,6 @@ def model_metrics(X, y, classifier, class_names):
     metrics = classification_report(y_test, y_pred)
 
     return plt, metrics
-
 
 def svc_param_selection(X, y, nfolds, n):
     Cs = [0.001, 0.01, 0.1, 1, 10]
@@ -193,3 +193,86 @@ def knn_Srandom (clf, X, y, base_accuracy, n_iter, k_folds=3):
     rf_random, improv = randS(clf, X, y, base_accuracy, n_iter, k_folds, random_grid)
 
     return rf_random, improv
+
+# Calibration analysis
+def plot_cal_curve(X, y, clfs):
+    # Split the data into a training set and a test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+    # Plot calibration plots
+
+    plt.figure(figsize=(18, 15));
+    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+    ax2 = plt.subplot2grid((3, 1), (2, 0))
+
+    ax1.plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
+    for clf, name in clfs:
+        clf.fit(X_train, y_train)
+        if hasattr(clf, "predict_proba"):
+            prob_pos = clf.predict_proba(X_test)[:, 1]
+        else:  # use decision function
+            prob_pos = clf.decision_function(X_test)
+            prob_pos = \
+                (prob_pos - prob_pos.min()) / (prob_pos.max() - prob_pos.min())
+        fraction_of_positives, mean_predicted_value = \
+            calibration_curve(y_test, prob_pos, n_bins=10)
+
+        ax1.plot(mean_predicted_value, fraction_of_positives, "s-",
+                 label="%s" % (name,))
+
+        ax2.hist(prob_pos, range=(0, 1), bins=10, label=name,
+                 histtype="step", lw=2)
+
+    ax1.set_ylabel("Fraction of positives")
+    ax1.set_ylim([-0.05, 1.05])
+    ax1.legend(loc="lower right")
+    ax1.set_title('Calibration plots  (reliability curve)')
+
+    ax2.set_xlabel("Mean predicted value")
+    ax2.set_ylabel("Count")
+    ax2.legend(loc="upper center", ncol=2)
+
+    plt.tight_layout()
+    return plt
+
+# Classification and ROC analysis
+def roc_metrics(X, y, classifier, n_splits):
+    # Run classifier with cross-validation and plot ROC curves
+    cv = StratifiedKFold(n_splits=n_splits)
+
+    tprs = []
+    aucs = []
+    mean_fpr = np.linspace(0, 1, 100)
+
+    fig, ax = plt.subplots()
+    for i, (train, test) in enumerate(cv.split(X, y)):
+        classifier.fit(X.iloc[train], y[train])
+        viz = plot_roc_curve(classifier, X.iloc[test], y[test],
+                             name='ROC fold {}'.format(i),
+                             alpha=0.3, lw=1, ax=ax)
+        interp_tpr = interp(mean_fpr, viz.fpr, viz.tpr)
+        interp_tpr[0] = 0.0
+        tprs.append(interp_tpr)
+        aucs.append(viz.roc_auc)
+
+    ax.plot([0, 1], [0, 1], linestyle='--', lw=2, color='r',
+            label='Chance', alpha=.8)
+
+    mean_tpr = np.mean(tprs, axis=0)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    std_auc = np.std(aucs)
+    ax.plot(mean_fpr, mean_tpr, color='b',
+            label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),
+            lw=2, alpha=.8)
+
+    std_tpr = np.std(tprs, axis=0)
+    tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
+    tprs_lower = np.maximum(mean_tpr - std_tpr, 0)
+    ax.fill_between(mean_fpr, tprs_lower, tprs_upper, color='grey', alpha=.2,
+                    label=r'$\pm$ 1 std. dev.')
+
+    ax.set(xlim=[-0.05, 1.05], ylim=[-0.05, 1.05],
+           title="Receiver operating characteristic example")
+    ax.legend(loc="lower right")
+    return plt
